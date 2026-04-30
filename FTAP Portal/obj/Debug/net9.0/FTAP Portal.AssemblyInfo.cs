712ae5ac3fe6a1cd22353f3019be3af3db84202d
Elias <elias.lavrich@gmail.com>
2026-04-30 00:37:15 -0400

    styled the leaderboard a little more

--- FTAP Portal/obj/Debug/net9.0/FTAP Portal.AssemblyInfo.cs
+++ FTAP Portal/obj/Debug/net9.0/FTAP Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e0d50afcea434622878d230cee384886476433")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7662796d2085aacf304772dff4b5e29a90bbeea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
