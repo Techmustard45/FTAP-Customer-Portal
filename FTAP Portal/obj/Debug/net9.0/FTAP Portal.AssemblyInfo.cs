97abd7e5321aa6060c54a22f48fb689fcf674784
Elias <elias.lavrich@gmail.com>
2026-05-15 11:46:17 -0400

    removed .idea directory

--- FTAP Portal/obj/Debug/net9.0/FTAP Portal.AssemblyInfo.cs
+++ FTAP Portal/obj/Debug/net9.0/FTAP Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeee8b9f26f8466847fbc60944bbc322d30ecaf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f852c96813840fb949c8ea5dc37f6bff50ee7f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FTAP Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
